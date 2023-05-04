7155e1923c510b62ac958f6a4e3631d4051d3b20
Sam Arch <samarch@sams-mbp-5.wifi.local.cmu.edu>
2023-05-04 17:52:05 -0400

    Add missing case statement

--- src/planner/logical_operator.cpp
+++ src/planner/logical_operator.cpp
@@ -355,6 +355,7 @@ unique_ptr<LogicalOperator> LogicalOperator::Deserialize(Deserializer &deseriali
 	case LogicalOperatorType::LOGICAL_PIVOT:
 		result = LogicalPivot::Deserialize(state, reader);
 		break;
+	case LogicalOperatorType::LOGICAL_DEPENDENT_JOIN:
 	case LogicalOperatorType::LOGICAL_INVALID:
 		/* no default here to trigger a warning if we forget to implement deserialize for a new operator */
 		throw SerializationException("Invalid type for operator deserialization");
