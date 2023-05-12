4a25f13d74d259f944706c3a06a83bec1a38477f
Mayank-Baranwal <baranwalm2002@gmail.com>
2023-05-12 00:22:02 -0400

    Added comments for logical_dependent_join.hpp

--- src/include/duckdb/planner/operator/logical_dependent_join.hpp
+++ src/include/duckdb/planner/operator/logical_dependent_join.hpp
@@ -3,8 +3,17 @@
 //
 // duckdb/planner/operator/logical_dependent_join.hpp
 //
+// logical_dependent_join represents a logical operator for lateral joins that
+// is planned buts not yet flattened
+//
+// This construct only exists during planning and should not exist in the plan
+// once flattening is complete. Although the same information can be kept in the
+// join itself, creating a new construct makes the code cleaner and easier to
+// understand.
 //
 //===----------------------------------------------------------------------===//
+
+
 
 #pragma once
 
@@ -21,7 +30,6 @@ public:
 
 	//! The conditions of the join
 	unique_ptr<Expression> join_condition;
-	//! Used for duplicate-eliminated joins
 	vector<CorrelatedColumnInfo> correlated_columns;
 
 public:
